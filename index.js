1a2f11f820336b135ed67f16276c74ec02be2427
mastaryan <58410246+mastaryan@users.noreply.github.com>
2019-12-17 23:54:55 -0500

    Task 3

--- index.js
+++ index.js
@@ -117,6 +117,15 @@ Car.prototype.drive = function(distance) {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+
+}
+
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
   
 }
 
