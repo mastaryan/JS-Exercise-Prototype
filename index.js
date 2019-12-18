140719bb4b7e9e462d21e64480bd85124cd48d9b
mastaryan <58410246+mastaryan@users.noreply.github.com>
2019-12-17 23:43:10 -0500

    Task 2 w/o stretch

--- index.js
+++ index.js
@@ -74,7 +74,15 @@ Person.prototype.toString = function() {
 */
 
 function Car(model, milesPerGallon) {
-  
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+
+}
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
 }
 
 /*
